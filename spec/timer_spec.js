d378171c36dbbe69fc69b4fbf640b7ef21b9a5d4
Abraham Fong <abisfong@ucsc.edu>
2021-10-30 05:59:02 -0700

    Begin testing the Timer class
    Since the document is the Jasmine HTML file, HTML elements have
    to be created manually for the Timer (and in any other tests).
    
    At the top of the file, the timerEl and timeEl are both mock
    elements for the timer HTML elements in index.html.

---
+++ spec/timer_spec.js
@@ -0,0 +1,9 @@
+describe("Timer", function() {
+  let timerEl = document.createElement('div');
+  let timeEl = document.createElement('div');
+  let timer;
+  timerEl.classList.add('timer');
+  timeEl.classList.add('time');
+  timerEl.appendChild(timeEl);
+
+});
