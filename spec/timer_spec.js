65169f10cf57452cd2695900b5e0ae3c91fd841e
Abraham Fong <abisfong@ucsc.edu>
2021-10-30 06:02:06 -0700

    Test the constructor for Timer

--- spec/timer_spec.js
+++ spec/timer_spec.js
@@ -5,5 +5,19 @@ describe("Timer", function() {
   timerEl.classList.add('timer');
   timeEl.classList.add('time');
   timerEl.appendChild(timeEl);
-
+  
+  describe("#contructor", function() {
+    it("should initialize timer object at 00:00:00 when no time is passed in", function () {
+      timer = new Timer(timerEl);
+      expect(timer.time).toBe(0);
+      expect(timer.timeEl.innerText).toBe("00:00:00");
+    });
+    
+    it("should initialize timer object at given time when time is passed in", function () {
+      timer = new Timer(timerEl, 300);
+      expect(timer.time).toBe(300);
+      expect(timer.timeEl.innerText).toBe("00:05:00");
+      console.log(timer.timerEl);
+    });
+  });
 });
