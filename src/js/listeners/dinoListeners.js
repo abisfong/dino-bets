7a3512d374aaec5bf86f209c653679bbb1b9de6a
Abraham Fong <abisfong@ucsc.edu>
2021-11-04 21:40:12 -0700

    Build skeleton for Dino movement listeners

--- src/js/listeners/dinoListeners.js
+++ src/js/listeners/dinoListeners.js
@@ -1,5 +1,6 @@
-export default function addDinoEventListeners(dino) {
+export default function addDinoEventListeners() {
   addRunAnimationEventListeners();
+  addRunMovementEventListeners();
 }
 
 function addRunAnimationEventListeners() {
@@ -9,20 +10,31 @@ function addRunAnimationEventListeners() {
 }
 
 function startDinoRunFrameCycle(event) {
-  let dino = event.dino;
-  console.log('starting running animation', dino);
-  dino.timeoutIDs.run = setInterval(function () {
+  const dino = event.dino;
+  dino.timeoutIDs.runAnimation = setInterval(function () {
     dino.cycleRunningFrame();
   }, 100 / dino.speed)
 }
 
 function stopDinoRunFrameCycle(event) {
-  let dino = event.dino;
-  clearInterval(dino.timeoutIDs.run);
+  const dino = event.dino;
+  clearInterval(dino.timeoutIDs.runAnimation);
 }
 
 function addRunMovementEventListeners() {
   let canvasEl = document.getElementById('canvas');
-  canvasEl.addEventListener('startDinoRunMovement', startDinoRunFrameCycle);
-  canvasEl.addEventListener('stopDinoRunMovement', stopDinoRunFrameCycle);
+  canvasEl.addEventListener('startDinoRunMovement', startDinoRunMovement);
+  canvasEl.addEventListener('stopDinoRunMovement', stopDinoRunMovement);
+}
+
+function startDinoRunMovement(event) {
+  const dino = event.dino;
+  dino.timeoutIDs.runMovement = setInterval(function () {
+    dino.randomXMovement();
+  }, 50);
+}
+
+function stopDinoRunMovement(event) {
+  const dino = event.dino;
+  clearInterval(dino.timeoutIDs.runMovement);
 }
