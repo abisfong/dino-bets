fbcf371135783e810ca6c55b5f852d3458c12630
Abraham Fong <abisfong@ucsc.edu>
2021-11-06 17:11:05 -0700

    Refactor Timer to not use listeners

--- src/js/models/timer.js
+++ src/js/models/timer.js
@@ -11,13 +11,12 @@ export default class Timer {
     this.resetEl = timerEl.querySelector('#reset-btn');
     this.inputEls = this.timeEl.children;
     this.printTime();
-    attachTimerEventListeners(this);
   }
 
   printTime() {
-    let hoursEl = this.timeEl.children[0];
-    let minutesEl = this.timeEl.children[1];
-    let secondsEl = this.timeEl.children[2];
+    const hoursEl = this.timeEl.children[0];
+    const minutesEl = this.timeEl.children[1];
+    const secondsEl = this.timeEl.children[2];
     this.updateTime();
     hoursEl.value = this.hours < 10 ? '0' + this.hours : this.hours;
     minutesEl.value = this.minutes < 10 ? '0' + this.minutes : this.minutes;
@@ -39,6 +38,8 @@ export default class Timer {
     this.hours = Math.floor(this.remainingTime / (60 * 60));
     this.minutes = Math.floor((this.remainingTime % (3600)) / 60);
     this.seconds = this.remainingTime % 60;
+    this.reset();
+    blurTimerInputFocus(this);
   }
 
   start() {
@@ -46,6 +47,7 @@ export default class Timer {
     this.startPauseEl.classList.add('pause');
     this.startPauseEl.innerHTML = 'PAUSE'
     this.timeIntervalId = setInterval(this.tick.bind(this), 1000);
+    setTimerInputReadOnly(this, true);
   }
 
   pause() {
@@ -59,6 +61,7 @@ export default class Timer {
     this.timeElapsed = 0;
     this.pause();
     this.printTime();
+    setTimerInputReadOnly(this, false);
   }
 
   setTime(time) {
@@ -66,21 +69,21 @@ export default class Timer {
   }
 
   setHours(amount) {
-    let currentHoursInSecs = this.hours * 60 *  60;
-    let hoursToAdd = amount * 60 * 60;
+    const currentHoursInSecs = this.hours * 60 *  60;
+    const hoursToAdd = amount * 60 * 60;
     this.time -= currentHoursInSecs;
     this.time += hoursToAdd;
   }
 
   setMinutes(amount) {
-    let currentMinutesInSecs = this.minutes * 60;
-    let minutesToAdd = amount * 60;
+    const currentMinutesInSecs = this.minutes * 60;
+    const minutesToAdd = amount * 60;
     this.time -= currentMinutesInSecs;
     this.time += minutesToAdd;
   }
 
   setSeconds(amount) {
-    let currentSeconds = this.seconds;
+    const currentSeconds = this.seconds;
     this.time -= currentSeconds;
     this.time += amount;
   }
@@ -89,15 +92,12 @@ export default class Timer {
 function addTimeFromInput(timer) {
   const inputEls = timer.inputEls;
   const setTimeFromInputCurry = curry(setTimeFromInput, timer, inputEls.length);
-
+  
   for(let i = 0; i < inputEls.length; i++) {
     let input = inputEls[i].value;
     if (isValidTimeInput(input)) { 
       let timeAmount = parseInt(input);
       setTimeFromInputCurry(timeAmount);
-    } else {
-      timer.reset();
-      return;
     }
   }
 }
@@ -105,7 +105,6 @@ function addTimeFromInput(timer) {
 function setTimeFromInput(timer) {
   const input = Array.from(arguments);
   const setTimeMethodNames = ['setHours', 'setMinutes', 'setSeconds'];
-
   for (let i = 0; i < 3; i++)
     this[setTimeMethodNames[i]](input[i]);
 }
@@ -114,6 +113,15 @@ function isValidTimeInput(value) {
   return /^\d+$/.test(value) && value <= 99;
 }
 
-function blurFocus() {
-  document.activeElement.blur();
+function blurTimerInputFocus(timer) {
+  const activeEl = document.activeElement;
+  const inputEls = timer.inputEls;
+  if (inputEls.includes(activeEl))
+    document.activeElement.blur();
+}
+
+function setTimerInputReadOnly(timer, value) {
+  const inputEls = timer.inputEls;
+  for (let i = 0; i < inputEls.length; i++)
+    inputEls[i].readOnly = value;
 }
