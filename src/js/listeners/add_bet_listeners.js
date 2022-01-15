8ede90dc2da1371b6139fe7ed95158886cbc214a
Abraham Fong <abisfong@ucsc.edu>
2022-01-15 14:20:02 -0800

    Change background color of bet controller on selection change

--- src/js/listeners/add_bet_listeners.js
+++ src/js/listeners/add_bet_listeners.js
@@ -1,35 +1,31 @@
-// const betTypes = ['WIN', 'SHOW', 'SEQUENCE'];
-// let currentTypeSelection = 0;
-// let currentSubTypeSelection = 0;
+const selectionColors = ['green', 'red', 'yellow', 'purple'];
+let selection = 0;
 
 export default function addBetListeners() {
   addPrevListener();
   addNextListener();
-  // addTypeButtonListener();
   addSubmitListener();
 }
 
 function addPrevListener() {
   const prevEl = document.querySelector('#prev');
+  prevEl.addEventListener('click', rotateSelection(-1));
 }
 
 function addNextListener() {
   const nextEl = document.querySelector('#next');
+  nextEl.addEventListener('click', rotateSelection(1));
 }
 
-// function 
-
-// function addTypeButtonListener() {
-//   const typeButtonEl = document.querySelector('#bet-type-button');
-//   typeButtonEl.addEventListener('click', rotateTypeSelection());
-// }
-
-// function rotateTypeSelection() {
-//   return e => {
-//     e.target.innerText = `TYPE: ${betTypes[++currentTypeSelection % 3]}`;
-//     currentSubTypeSelection = 0;
-//   };
-// }
+function rotateSelection(dir) {
+  const betControllerEl = document.querySelector('#bet-controller');
+  
+  return e => {
+    betControllerEl.classList.remove(selectionColors[selection]);
+    selection = (((selection + dir) % 4 ) + 4 ) % 4
+    betControllerEl.classList.add(selectionColors[selection]);
+  };
+}
 
 function addSubmitListener() {
   const submitButtonEl = document.querySelector('#bet-submit-button');
