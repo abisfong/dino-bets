e08343869248e5e42494370735b3b4a4d88f120f
Abraham Fong <abisfong@ucsc.edu>
2022-01-14 20:59:54 -0800

    Create amount element listener

--- src/js/listeners/index.js
+++ src/js/listeners/index.js
@@ -1,9 +1,11 @@
 import addTimerListeners from "./add_timer_listeners";
 import addGameListeners from "./add_game_listeners";
+import addAmountInputListener from "./add_amount_input_lisenter";
 
 function init({ game, timer }) {
   addTimerListeners(timer);
   addGameListeners(game);
+  addAmountInputListener();
 }
 
 export {
