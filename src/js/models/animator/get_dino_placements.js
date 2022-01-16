bb88cdf68ecb601d884b9dc9c2a5b29c035b4e18
Abraham Fong <abisfong@ucsc.edu>
2022-01-16 13:41:10 -0800

    Create util function to calculate placements of dinos

---
+++ src/js/models/animator/get_dino_placements.js
@@ -0,0 +1,13 @@
+export default function getDinoPlacements(dinoSprites) {
+  const placements = [];
+
+  for(let i = 0; i < dinoSprites.length; i++) {
+    const dinoSprite = dinoSprites[i];
+    const dinoPosX = dinoSprite.posX + dinoSprite.posXDelta;
+    const placement = 0;
+    for(let j = 0; j < dinoSprites.length; j++)
+      if (dinoPosX < dinoSprites[j].posX + dinoSprites[j].posXDelta)
+        placement++;
+    placements[i] = placement + 1;
+  }
+}
