0de6c3a73d116dc868e90a7e60dde997d3ca6dd4
Abraham Fong <abisfong@ucsc.edu>
2022-01-17 21:17:24 -0800

    Attempt to fix path issue with audio and image files

--- src/js/models/foley.js
+++ src/js/models/foley.js
@@ -2,14 +2,14 @@ export default class Foley {
   constructor() {
     this.src;
     this.audios = {
-      placedBet: new Audio('/dist/assets/audios/placed-bet.wav'),
-      canceledBet: new Audio('/dist/assets/audios/canceled-bet.wav'),
-      invalidBet: new Audio('/dist/assets/audios/invalid-bet.mov'),
-      betViewToggle: new Audio('/dist/assets/audios/bet-view-toggle.wav'),
-      betSelection: new Audio('/dist/assets/audios/bet-selection.wav'),
-      timerBtnClick: new Audio('/dist/assets/audios/timer-btn-click.wav'),
-      positiveBetReturn: new Audio('/dist/assets/audios/positive-bet-return.wav'),
-      updateAmount: new Audio('/dist/assets/audios/update-amount.wav'),
+      placedBet: new Audio('./dist/assets/audios/placed-bet.wav'),
+      canceledBet: new Audio('./dist/assets/audios/canceled-bet.wav'),
+      invalidBet: new Audio('./dist/assets/audios/invalid-bet.mov'),
+      betViewToggle: new Audio('./dist/assets/audios/bet-view-toggle.wav'),
+      betSelection: new Audio('./dist/assets/audios/bet-selection.wav'),
+      timerBtnClick: new Audio('./dist/assets/audios/timer-btn-click.wav'),
+      positiveBetReturn: new Audio('./dist/assets/audios/positive-bet-return.wav'),
+      updateAmount: new Audio('./dist/assets/audios/update-amount.wav'),
     }
   }
 
