b45b87438e4ddcd1493386629cb01d322b956856
Abraham Fong <abisfong@ucsc.edu>
2022-01-17 20:16:25 -0800

    Play sound effect on timer button click

--- src/js/listeners/add_game_listeners.js
+++ src/js/listeners/add_game_listeners.js
@@ -13,15 +13,23 @@ function addStartPauseButtonListener(game) {
 }
 
 function createStartPauseButtonCallback(game) {
+  const foley = game.foley;
+
   return function toggleStartPause() {
+    foley.playSoundEffectFor('timerBtnClick');
     if (game.time() <= 0) return;
     game.state.running ? game.pause() : game.start();
   }
 }
 
 function addResetButtonListener(game) {
+  const foley = game.foley;
   const resetEl = document.querySelector('#reset-btn');
-  resetEl.addEventListener('click', game.reset.bind(game));
+
+  resetEl.addEventListener('click', () => {
+    foley.playSoundEffectFor('timerBtnClick');
+    game.reset();
+  });
 }
 
 function addRaceCompleteListener(game) {
